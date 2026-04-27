86498b89f340a917912c00310e9ea1d42be55853
leallsweep <idnksolution@gmail.com>
2026-04-27 12:50:43 +0300

    boot.asm fixed

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -4,10 +4,24 @@ dd 0x1BADB002
 dd 0x0
 dd -(0x1BADB002)
 
+section .note.GNU-stack noalloc noexec nowrite progbits
+
+section .bss
+align 16
+stack:
+resb 16384
+
 section .text
 global _start
 extern kernel_main
 
 _start:
+    cli
+
+    mov esp, stack + 16384
+
     call kernel_main
-    jmp $
+
+.hang:
+    hlt
+    jmp .hang
