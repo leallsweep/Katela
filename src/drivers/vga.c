92115073766f7f43269cf1a1d713bb844abb34f7
leallsweep <idnksolution@gmail.com>
2026-05-17 09:42:42 +0300

    Added frame and window addition commands

--- src/drivers/vga.c
+++ src/drivers/vga.c
@@ -86,3 +86,79 @@ void clear() {
     pos = 0;
     update_cursor();
 }
+
+void frame(int x, int y, int width, int height, unsigned char color) {
+    int start_pos = (y * 80 + x) * 2;
+    
+    for (int col = 1; col < width - 1; col++) {
+        int top_pos = start_pos + col * 2;
+        int bottom_pos = start_pos + ((height - 1) * 80 * 2) + col * 2;
+        video[top_pos] = 0xCD;
+        video[top_pos + 1] = color;
+        video[bottom_pos] = 0xCD;
+        video[bottom_pos + 1] = color;
+    }
+    
+    for (int row = 1; row < height - 1; row++) {
+        int left_pos = start_pos + (row * 80 * 2);
+        int right_pos = start_pos + (row * 80 * 2) + ((width - 1) * 2);
+        video[left_pos] = 0xBA;
+        video[left_pos + 1] = color;
+        video[right_pos] = 0xBA;
+        video[right_pos + 1] = color;
+    }
+    
+    int tl = start_pos;
+    int tr = start_pos + ((width - 1) * 2);
+    int bl = start_pos + ((height - 1) * 80 * 2);
+    int br = start_pos + ((height - 1) * 80 * 2) + ((width - 1) * 2);
+    
+    video[tl] = 0xC9;
+    video[tl + 1] = color;
+    video[tr] = 0xBB;
+    video[tr + 1] = color;
+    video[bl] = 0xC8;
+    video[bl + 1] = color;
+    video[br] = 0xBC;
+    video[br + 1] = color;
+}
+
+void window(int x, int y, int width, int height, const char* title, const char* content, unsigned char title_color, unsigned char border_color, unsigned char content_color) {
+    frame(x, y, width, height, border_color);
+    
+    if (title != 0) {
+        int title_len = 0;
+        while (title[title_len] != 0) title_len++;
+        
+        int start_x = x + (width - title_len) / 2;
+        int pos_y = y;
+        
+        int start_pos = (pos_y * 80 + start_x) * 2;
+        
+        for (int i = 0; title[i] != 0; i++) {
+            video[start_pos + i * 2] = title[i];
+            video[start_pos + i * 2 + 1] = title_color;
+        }
+    }
+    
+    for (int row = 1; row < height - 1; row++) {
+        square(x + 1, y + row, width - 2, 1, 0x00);
+    }
+    
+    if (content != 0) {
+        int content_x = x + 2;
+        int content_y = y + 2;
+        int content_pos = (content_y * 80 + content_x) * 2;
+        
+        for (int i = 0; content[i] != 0; i++) {
+            if (content[i] == '\n') {
+                content_y++;
+                content_pos = (content_y * 80 + content_x) * 2;
+            } else {
+                video[content_pos] = content[i];
+                video[content_pos + 1] = content_color;
+                content_pos += 2;
+            }
+        }
+    }
+}
--- src/drivers/vga.h
+++ src/drivers/vga.h
@@ -5,5 +5,7 @@ void print(const char* str);
 void backspace();
 void clear();
 void square(int x, int y, int width, int height, unsigned char color);
+void frame(int x, int y, int width, int height, unsigned char color);
+void window(int x, int y, int width, int height, const char* title, const char* content, unsigned char title_color, unsigned char border_color, unsigned char content_color);
 
 #endif
